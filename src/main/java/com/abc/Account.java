ac054bb41bb65cc5e25d022a20ed810f74954599
Alex <>
2015-10-02 22:25:50 -0400

    asas

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -3,25 +3,43 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+
 public class Account {
 
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
+    public static final int MAXI_SAVINGS_ACCRUE = 3;
+    
+    public static int promotionDayRule = 10; 	// In most cases will come from config file or external source. 
+    											// For the exercise it is hard coded to match 10 day requirement.
 
     private final int accountType;
     public List<Transaction> transactions;
+    private double accountBalance;
+    private int accountId;
 
-    public Account(int accountType) {
+	public Account(int accountType, int accId) {
+		this.accountId = accId;
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
     }
 
+    public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
             transactions.add(new Transaction(amount));
+            accountBalance += amount;
         }
     }
 
@@ -30,11 +48,16 @@ public void withdraw(double amount) {
         throw new IllegalArgumentException("amount must be greater than zero");
     } else {
         transactions.add(new Transaction(-amount));
+        accountBalance += amount;
     }
 }
 
     public double interestEarned() {
         double amount = sumTransactions();
+        DateTime dt = new DateTime();
+    	DateTime earlierDate = dt.minusDays(promotionDayRule);
+    	DateTime tranDate;
+    	Boolean promoRate;
         switch(accountType){
             case SAVINGS:
                 if (amount <= 1000)
@@ -45,11 +68,36 @@ public void withdraw(double amount) {
 //                if (amount <= 4000)
 //                    return 20;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+            	promoRate = false;
+
+
+            	for (Transaction tran : transactions){
+            		tranDate = new DateTime(tran.getTransactionDate());
+            		promoRate = tranDate.isBefore(earlierDate) && tran.getTransactionAmount() > 0? true : false;
+            	}
+            	return amount = promoRate ? amount * 0.05 : amount * 0.001;
+            	
+            case MAXI_SAVINGS_ACCRUE:
+            	promoRate = false;
+            	Double totalInterest = 0.0;
+            	
+            	for (Transaction tran : transactions){
+            		DateTime singleTranDate = new DateTime(tran.getTransactionDate());
+            		long dayDiffMills = dt.getMillis() - singleTranDate.getMillis();
+            		
+            		// Doesn't include today
+            		int dayDiff = (int)(dayDiffMills / (1000*60*60*24));
+
+            		
+            		tranDate = new DateTime(tran.getTransactionDate());
+            		promoRate = tranDate.isBefore(earlierDate) && tran.getTransactionAmount() > 0? true : false;
+            		double rate = promoRate? 0.05 : 0.001;
+            		for (int i = 0; i < dayDiff; i++){
+            			totalInterest += tran.getTransactionAmount() * rate / 365; // 365 makes the rate per year. 
+            		}
+            	}
+            	return Math.round(totalInterest*100)/100.00;
+
             default:
                 return amount * 0.001;
         }
@@ -67,7 +115,15 @@ public void withdraw(double amount) {
     }
 
     public int getAccountType() {
-        return accountType;
-    }
+		return accountType;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
 
 }
--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -3,6 +3,8 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.abc.exceptions.IncorrectAccNumException;
+
 import static java.lang.Math.abs;
 
 public class Customer {
@@ -75,4 +77,44 @@ public class Customer {
     private String toDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
+    
+    public List<Account> getAllAccounts(){
+    	return accounts;
+    }
+    
+    public void accountTransfer(int srcAccNum, int tgtAccNum, double amount) throws IncorrectAccNumException{
+    	Account srcAcc = null;
+    	Account tgtAcc = null;
+    	for (Account acc : accounts){
+    		if (acc.getAccountId() == srcAccNum){
+    			srcAcc = acc;
+    		}
+    		if (acc.getAccountId() == tgtAccNum){
+    			tgtAcc = acc;
+    		}    		
+    		// In a realistic environment I will do it as a single transaction, most likely in a stored procedure,
+    		// but for this exercise will do one operation at a time
+    	}
+    	if (srcAcc == null){
+    		throw new IncorrectAccNumException("Incorrect source account number");
+    	}
+    	if (tgtAcc == null){
+    		throw new IncorrectAccNumException("Incorrect target account number");
+    	}
+		if (srcAcc != null && tgtAcc != null){
+			srcAcc.withdraw(amount);
+			tgtAcc.deposit(amount);
+		}
+    }
+    
+	public Account getAccountById (int id){
+		Account account = null;
+		for (Account acc: accounts){
+			if (acc.getAccountId() == id){
+				account = acc;
+			}
+		}
+		return account;
+	}
+    
 }
